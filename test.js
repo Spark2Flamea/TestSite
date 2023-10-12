16927af9e3e17422408fd41776fdb0f07fee40db
Spark2Flamea <sparkdevs1220a@outlook.com>
2023-10-12 16:48:14 -0500

    Fleshing out the test file, almost ready for them to be taken offline and onto the stack

--- test.js
+++ test.js
@@ -9,13 +9,17 @@ Scope   Test js file
 function debug(x){
     console.log("Debugging " + x);
 };
-function getTruck(){
-    x = document.getElementById("truckInput").value;
-    debug(x);
-};
 
 
 
+/*This function simply takes you back to the home page
+when you click on the logo of the company in the toolbar*/
+function homeLink(){
+    window.location = 'file:///media/fuse/crostini_3648260f9aefcc0775338a12dd23dce1bc45f225_termina_penguin/Testing/test.html';
+}
+function aboutmeLink(){
+    window.location = 'file:///media/fuse/crostini_3648260f9aefcc0775338a12dd23dce1bc45f225_termina_penguin/Testing/test.html';
+}
 
 
 
